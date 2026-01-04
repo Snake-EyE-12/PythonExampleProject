4c71d645dbf4b672514361ad8480da846856e54a
Snake-EyE-12 <chase.guymon@q.com>
2026-01-04 16:40:04 -0700

    Update writer.py

--- writer.py
+++ writer.py
@@ -3,7 +3,18 @@ class Generator:
         return "give @s minecraft:" + text
 
     def Roll(self, size):
-        return "random roll 1.." + size
+        return "execute store result storage random:roll value int 1 run random value 1.." + size
+    def Value(self, value):
+        return "execute if data storage random:roll {value:"+value+"}"
+
+    def TreeDepth(self, depth, group): #depth0group0
+        return "function game:depth"+depth+"group"+group
+
+    def FullRandomGive(self, value, text):
+        return self.Value(self, value) + " run " + self.Give(self, text)
+
+    def FullRandomDepth(self, value, depth, group):
+        return self.Value(self, value) + " run " + self.TreeDepth(self, depth, group)
 
     def Generate(self, pattern):
         return "done"
